fe751bb3aa1abe2dd47bdb63f3e7a09afd6292a0
Martin Zink <martinzink@apache.org>
2026-02-12 15:31:59 +0100

    newer

--- minifi-native/src/lib.rs
+++ minifi-native/src/lib.rs
@@ -30,6 +30,23 @@ pub use mock::{
 };
 
 pub use minifi_native_sys as sys;
+use minifi_native_sys::{
+    MINIFI_API_MAJOR_VERSION, MINIFI_API_MINOR_VERSION, MINIFI_API_PATCH_VERSION,
+};
+
+#[unsafe(no_mangle)]
+#[cfg_attr(target_os = "linux", unsafe(link_section = ".rodata"))]
+#[cfg_attr(target_os = "macos", unsafe(link_section = "__DATA,__const"))]
+#[cfg_attr(target_os = "windows", unsafe(link_section = ".rdata"))]
+pub static MINIFI_C_API_VERSION: &str = const_format::concatcp!(
+    "MINIFI_API_VERSION=[",
+    MINIFI_API_MAJOR_VERSION,
+    ".",
+    MINIFI_API_MINOR_VERSION,
+    ".",
+    MINIFI_API_PATCH_VERSION,
+    "]"
+);
 
 #[macro_export]
 macro_rules! declare_minifi_extension {
@@ -74,7 +91,7 @@ macro_rules! declare_minifi_extension {
                     controller_services_ptr: controller_list.get_controller_service_ptr(),
                 };
 
-                minifi_native::sys::MinifiCreateExtension(minifi_native::sys::MinifiCApiVersion_0_1(), &extension_create_info)
+                minifi_native::sys::MinifiCreateExtension_0_1(minifi_native::MINIFI_C_API_VERSION.as_minifi_c_type(), &extension_create_info)
             }
         }
     };
